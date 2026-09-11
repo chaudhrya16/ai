fd74039a4bce82069cd7d09a1cfb518700dd8844
chaudhrya16 <chaudhry_a@yahoo.com>
2026-09-11 08:22:35 -0700

    Implement basic Gradio chat interface

---
+++ app.py
@@ -0,0 +1,17 @@
+import gradio as gr
+import os
+
+
+def respond(message, history):
+    result = f"You said: {message}\nAnd I said that I am just learning AI"
+    return result
+
+
+gr.ChatInterface(
+    fn=respond,
+    title="My First Space",
+    description="This is my first Space. I am learning AI and this is my first Space."
+).launch(
+    server_name="0.0.0.0",
+    server_port=int(os.getenv("PORT", 9050))
+)
